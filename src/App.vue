58d4d7969fe96e3d74cfc5f542567921464310bd
Ruslan <ruslan_feoktistov@mail.ru>
2023-09-08 22:31:43 +0400

    sdd src

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <IndexComponents/>
+    <BlogComponents/>
+    <BlogdetalisComponents/>
+   <CategoryComponents/>
+   <ProjectDetalisComponent/>
+  </template>
+  
+  <script>
+  
+  import IndexComponents from './components/IndexComponents.vue';
+  import BlogComponents from './components/BlogComponents.vue';
+  import BlogdetalisComponents from './components/BlogdetalisComponents.vue';
+  import CategoryComponents from './components/CategoryComponents.vue';
+  import ProjectDetalisComponent from './components/ProjectDetalisComponent.vue';
+  
+  
+  export default {
+    name: 'App',
+    components: {
+      IndexComponents,
+      BlogComponents,
+      BlogdetalisComponents,
+      CategoryComponents,
+      ProjectDetalisComponent
+  }
+  }
+  </script>
+  
+  <style lang="scss">
+  #app {
+    
+  }
+  </style>
+  
